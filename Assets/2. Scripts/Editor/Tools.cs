aa7fac586dc10bcc08b3ffd6d729bd7b03740af1
djnjbjb <djnjbjb@gmail.com>
2025-01-23 16:39:44 +0800

    Tool(Convert Sprite To White) code, file path change

--- Assets/2. Scripts/Editor/Tools.cs
+++ Assets/2. Scripts/Editor/Tools.cs
@@ -10,17 +10,17 @@ namespace UnityDraw
         [MenuItem("Tools/Convert Sprite To White")]
         static void convert_sprite_to_white()
         {
-            string in_path = "Assets/3. GameAssets/Art/apple.png";
-            string out_path = Application.dataPath + "/3. GameAssets/Art/white_apple.png";
-            
+            string in_path = "Assets/3. GameAssets/Art/Apple.png";
+            string out_path = Application.dataPath + "/3. GameAssets/Art/White Apple.png";
+
             Sprite sprite = AssetDatabase.LoadAssetAtPath<Sprite>(in_path);
             Texture2D texture2d = sprite.texture;
-            
+
             Color32[] original_colors = texture2d.GetPixels32();
             Color32[] pixel_colors = original_colors.Select(
                     color => color.a != 0 ? new Color32(255,255,255,255) : new Color32(0, 0, 0, 0)
                 ).ToArray();
-            
+
             texture2d.SetPixels32(pixel_colors);
             texture2d.Apply();
             byte[] bytes = texture2d.EncodeToPNG();
